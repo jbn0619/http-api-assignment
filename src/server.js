f1f45226742f3cf0b6ca7b7b74ef2721222c376e
jbn0619 <jbn5379@g.rit.edu>
2021-09-18 13:38:07 -0400

    Continued work on api assignment

--- src/server.js
+++ src/server.js
@@ -1,54 +1,43 @@
 const http = require('http');
 const url = require('url');
-const query = require('querystring');
 const htmlHandler = require('./htmlResponses');
-const jsonHandler = require('./jsonResponses');
-const xmlHandler = require('./xmlResponses');
+const getHandler = require('./getResponses');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlDictionary = {
-    'GET': {
-        'application/json': {
-            '/': htmlHandler.getIndex,
-            '/style.css': htmlHandler.getCSS,
-            '/success': jsonHandler.getSuccess,
-            '/badRequest': jsonHandler.getBadRequest,
-            '/unauthorized': jsonHandler.getUnauthorized,
-            '/forbidden': jsonHandler.getForbidden,
-            '/internal': jsonHandler.getInternal,
-            '/notImplemented': jsonHandler.getNotImplemented,
-            notFound: jsonHanlder.notFound,
-        },
-        'text/xml': {
-            '/': htmlHandler.getIndex,
-            '/style.css': htmlHandler.getCSS,
-            '/success': xmlHandler.getSuccess,
-            '/badRequest': xmlHandler.getBadRequest,
-            '/unauthorized': xmlHandler.getUnauthorized,
-            '/forbidden': xmlHandler.getForbidden,
-            '/internal': xmlHandler.getInternal,
-            '/notImplemented': xmlHandler.getNotImplemented,
-            notFound: xmlHandler.notFound,
-        },
-    },
-};
-
-// Handles new information being posted to the application.
-const handlePost = (request, response, parsedUrl) => {
-
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': getHandler.getCSS,
+    '/success': getHandler.getSuccess,
+    '/badRequest': getHandler.getBadRequest,
+    '/unauthorized': getHandler.getUnauthorized,
+    '/forbidden': getHandler.getForbidden,
+    '/internal': getHandler.getInternal,
+    '/notImplemented': getHandler.getNotImplemented,
+    notFound: getHandler.getNotFound,
+  },
 };
 
 // Handles sending data from the application to the user.
-const handleGet = (request, response, parsedUrl) => {
+const handleGet = (request, response) => {
+  const parsedUrl = url.parse(request.url);
 
+  const acceptedTypes = request.headers.accept.split(',');
+  console.log(request.method);
+  console.log(acceptedTypes[0]);
+  console.log(parsedUrl.pathname);
+
+  if (urlDictionary[request.method][parsedUrl.pathname]) {
+    urlDictionary[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlDictionary[request.method].notFound(request, response);
+  }
 };
 
 // Parses URL and determines what kind of operation the API is handling.
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-
-    const acceptedTypes = request.headers.accept.split(',');
+  if (request.method === 'GET') handleGet(request, response);
 };
 
 http.createServer(onRequest).listen(port);
